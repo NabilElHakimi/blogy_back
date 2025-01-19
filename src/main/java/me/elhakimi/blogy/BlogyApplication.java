7b4c3a675b5c163cf03f89ec21fb7d35f563ab0f
EL-Nabil-Hakimi <hakimi.deve@gmail.com>
2025-01-19 20:50:12 +0100

    first commit

---
+++ src/main/java/me/elhakimi/blogy/BlogyApplication.java
@@ -0,0 +1,13 @@
+package me.elhakimi.blogy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/elhakimi/blogy/BlogyApplicationTests.java
@@ -0,0 +1,13 @@
+package me.elhakimi.blogy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
